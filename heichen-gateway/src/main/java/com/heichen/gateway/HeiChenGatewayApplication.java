0a95fdf275810b8fe86da24faefd7a7868826726
魏海球 <1196396546@qq.com>
2023-04-07 16:42:09 +0800

    第二次提交

---
+++ heichen-gateway/src/main/java/com/heichen/gateway/HeiChenGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.heichen.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class HeiChenGatewayApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(HeiChenGatewayApplication.class,args);
+    }
+}
